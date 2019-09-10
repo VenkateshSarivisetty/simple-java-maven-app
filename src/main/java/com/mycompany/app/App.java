7a48f50e1ea1a8482dbc1c2f12c949f806ed1b9e
root <root@ip-172-31-42-165.ec2.internal>
2019-09-10 18:03:26 +0000

    committed

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World Git and Jenkins and Maven!";
+    private final String message = "Hello World Git and Jenkins!";
 
     public App() {}
 
--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,7 +34,7 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("Hello Worldi Git and Jenkins and maven!" + System.getProperty("line.separator"), outContent.toString());
+            assertEquals("Hello Worldi Git and Jenkins!" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
             fail("\"message\" is not \"Hello World!\"");
         }
